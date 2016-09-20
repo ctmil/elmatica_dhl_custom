5c510de758d371f40ab3cd08c15828a5a0451f05
morrillo <gustavo.orrillo@moldeo.coop>
2016-09-20 15:08:46 -0300

    First commit

---
+++ __openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Elmatica - DHL Custom',
+    'category': 'Sales',
+    'version': '0.1',
+    'depends': ['multi_package_shipment'],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'installable': True,
+}
---
+++ models.py
@@ -0,0 +1,9 @@
+from openerp import api, models, exceptions, fields,  _
+import openerp.addons.decimal_precision as dp
+from openerp.osv import osv
+import logging
+import datetime
+
+_logger = logging.getLogger(__name__)
+
+

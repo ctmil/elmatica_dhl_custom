0a0e41022c733d51971f62ed1451062abdb4c8c4
morrillo <gustavo.orrillo@moldeo.coop>
2016-09-20 16:22:27 -0300

    Set default value for declared_value

--- models.py
+++ models.py
@@ -6,3 +6,17 @@ import datetime
 
 _logger = logging.getLogger(__name__)
 
+
+class multi_package_shipment_wizard(models.TransientModel):
+        _inherit= 'multi.package.shipment.wizard'
+
+        def default_get(self,cr,uid,fields,context):
+                res = super(multi_package_shipment_wizard,self).default_get(cr,uid,fields,context)
+                picking_ids=self.pool['stock.picking'].browse(cr,uid,context['active_ids'])
+		dhl_declared_value = 0
+		for picking_id in picking_ids:
+			if picking_id.dhl_declared_value > 0:
+				dhl_declared_value = dhl_declared_value + picking_id.dhl_declared_value
+		if dhl_declared_value > 0:
+			res['declared_value'] = dhl_declared_value
+		return res

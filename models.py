dce2f6005c2001b51815af273ee32f22de066864
morrillo <gustavo.orrillo@moldeo.coop>
2016-09-20 17:21:07 -0300

    Added origin to tree view

--- models.py
+++ models.py
@@ -20,3 +20,16 @@ class multi_package_shipment_wizard(models.TransientModel):
 		if dhl_declared_value > 0:
 			res['declared_value'] = dhl_declared_value
 		return res
+
+class multi_package_shipment(models.Model):
+	_inherit = 'multi.package.shipment'
+
+	@api.one
+	def _compute_origin(self):
+		return_value = ''
+		if self.picking_ids:
+			for picking_id in self.picking_ids:
+				return_value = return_value + picking_id.name
+		self.origin = return_value
+
+	origin = fields.Char(string='Origin',compute=_compute_origin)

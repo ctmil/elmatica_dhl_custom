1cf4cf34667973beeeb4081b19683b8b4377e92d
morrillo <gustavo.orrillo@moldeo.coop>
2016-09-20 15:32:10 -0300

    Added view

--- __openerp__.py
+++ __openerp__.py
@@ -2,8 +2,9 @@
     'name': 'Elmatica - DHL Custom',
     'category': 'Sales',
     'version': '0.1',
-    'depends': ['multi_package_shipment'],
+    'depends': ['multi_package_shipment','delivery'],
     'data': [
+	'delivery_view.xml'
     ],
     'demo': [
     ],
